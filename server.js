c5168a5b569120ac134d811c7187f553e6e968dc
saakethrajaram <s224611832@deakin.edu.au>
2024-08-08 14:45:37 +1000

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-var express = require("express")
-var app = express()
-app.use(express.static(__dirname+'/public'))
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-var port = process.env.port || 3000;
-app.listen(port,()=>{
-console.log("App listening to: "+port)
-})
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).send("Page not found");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
